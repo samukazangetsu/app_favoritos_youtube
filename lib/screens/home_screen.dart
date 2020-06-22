785047dc4d01f6ddd00960db6d1c7743d3e83c6d
Samuel Coutinho <samuel_alves_coutinho@hotmail.com>
2020-06-22 12:27:47 -0300

    app bar da homepage construida

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,8 +3,32 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          height: 70,
+          child: Image.asset("images/youtube-logo-light.png"),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.black,
+        actions: <Widget>[
+          Align(
+            alignment: Alignment.center,
+            child: Text("0"),
+          ),
+          IconButton(
+              icon: Icon(
+                Icons.star,
+              ),
+              onPressed: () {}),
+          IconButton(
+              icon: Icon(
+                Icons.search,
+              ),
+              onPressed: () {}),
+        ],
+      ),
+      body: Container(),
     );
   }
 }
