ad225b77f8e6569ef129164bedaad7904bc45104
Samuel Coutinho <samuel_alves_coutinho@hotmail.com>
2020-10-08 13:26:46 -0300

    finalização app

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -1,5 +1,7 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_youtube/flutter_youtube.dart';
+import 'package:fluttertube/api.dart';
 import 'package:fluttertube/blocs/favorite_bloc.dart';
 import 'package:fluttertube/models/video_model.dart';
 
@@ -21,7 +23,10 @@ class FavoritesScreen extends StatelessWidget {
           return ListView(
             children: snapshot.data.values.map((v) {
               return InkWell(
-                onTap: () {},
+                onTap: () {
+                  FlutterYoutube.playYoutubeVideoById(
+                      apiKey: API_KEY, videoId: v.id);
+                },
                 onLongPress: () {
                   blocFavorites.toggleFavorite(v);
                 },
--- lib/widgets/video_tile.dart
+++ lib/widgets/video_tile.dart
@@ -1,7 +1,10 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_youtube/flutter_youtube.dart';
 import 'package:fluttertube/blocs/favorite_bloc.dart';
 import 'package:fluttertube/models/video_model.dart';
+
+import '../api.dart';
 
 class VideoTile extends StatelessWidget {
   final Video video;
@@ -11,59 +14,64 @@ class VideoTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final bloc = BlocProvider.getBloc<FavoriteBloc>();
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 4.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          AspectRatio(
-            aspectRatio: 16.0 / 9.0,
-            child: Image.network(
-              video.thumb,
-              fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () {
+        FlutterYoutube.playYoutubeVideoById(apiKey: API_KEY, videoId: video.id);
+      },
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 4.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            AspectRatio(
+              aspectRatio: 16.0 / 9.0,
+              child: Image.network(
+                video.thumb,
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          Row(
-            children: [
-              Expanded(
-                  child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(8, 8, 8, 0),
-                    child: Text(
-                      video.title,
-                      style: TextStyle(color: Colors.white, fontSize: 16),
+            Row(
+              children: [
+                Expanded(
+                    child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(8, 8, 8, 0),
+                      child: Text(
+                        video.title,
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(8),
-                    child: Text(video.channel,
-                        style: TextStyle(color: Colors.white, fontSize: 14)),
-                  )
-                ],
-              )),
-              StreamBuilder<Map<String, Video>>(
-                  stream: bloc.outFav,
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData)
-                      return IconButton(
-                          icon: Icon(
-                            snapshot.data.containsKey(video.id)
-                                ? Icons.star
-                                : Icons.star_border,
-                            color: Colors.white,
-                            size: 30.0,
-                          ),
-                          onPressed: () {
-                            bloc.toggleFavorite(video);
-                          });
-                    else
-                      return CircularProgressIndicator();
-                  })
-            ],
-          )
-        ],
+                    Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Text(video.channel,
+                          style: TextStyle(color: Colors.white, fontSize: 14)),
+                    )
+                  ],
+                )),
+                StreamBuilder<Map<String, Video>>(
+                    stream: bloc.outFav,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData)
+                        return IconButton(
+                            icon: Icon(
+                              snapshot.data.containsKey(video.id)
+                                  ? Icons.star
+                                  : Icons.star_border,
+                              color: Colors.white,
+                              size: 30.0,
+                            ),
+                            onPressed: () {
+                              bloc.toggleFavorite(video);
+                            });
+                      else
+                        return CircularProgressIndicator();
+                    })
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
